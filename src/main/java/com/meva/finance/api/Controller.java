8201b091d89c69e1b7eac34922238dca4b9ce30b
Arthur Cardoso <arthur.fcardoso@hotmail.com>
2023-09-17 21:42:03 -0300

    #Estruturando api

--- src/main/java/com/meva/finance/api/Controller.java
+++ src/main/java/com/meva/finance/api/Controller.java
@@ -1,19 +1,36 @@
 
 package com.meva.finance.api;
 
+import com.meva.finance.dtos.CategoryDto;
+import com.meva.finance.entities.Category;
+import com.meva.finance.repositories.CategoryRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
+
 @Slf4j
 @RestController
+@RequestMapping("/category")
 public class Controller {
 
-	@PostMapping("/teste")
-	private void teste() throws Exception {
-		log.info("OK");
+    private final CategoryRepository categoryRepository;
 
-	}
+    public Controller(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+@GetMapping("/list")
+public List<CategoryDto> AllCategory() {
+        List<Category> categories = categoryRepository.findAll();
+
+        return  ;
+    }
 
 }
